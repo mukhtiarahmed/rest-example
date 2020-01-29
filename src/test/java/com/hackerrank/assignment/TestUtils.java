dabf70b465f89c11499273f0f791ae16d2d4319e
Mukhtiar Aahmed <mukhtiar.sherazi@gmail.com>
2020-01-29 11:42:54 +0500

    add test for create person post

--- src/test/java/com/hackerrank/assignment/TestUtils.java
+++ src/test/java/com/hackerrank/assignment/TestUtils.java
@@ -1,12 +1,18 @@
 package com.hackerrank.assignment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.hackerrank.assignment.domain.Colour;
 import com.hackerrank.assignment.domain.Hobby;
 import com.hackerrank.assignment.domain.Person;
+import com.hackerrank.assignment.dto.PersonDTO;
 import com.hackerrank.assignment.dto.SimplePersonDTO;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.http.MediaType;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
@@ -14,24 +20,28 @@ import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestUtils {
+    public static final String FIRST_NAME = "Ahmed";
+    public static  final String LAST_NAME = "Mukhtiar";
 
+    public static String convertObjectToJson(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
+    }
 
     public static Person createPerson() {
-        final String firstName = "Ahmed";
-        final String lastName = "Mukhtiar";
-        return createPerson(firstName, lastName);
+               return createPerson(FIRST_NAME, LAST_NAME);
     }
 
     public static Colour createColour(){
         final Colour colour = new Colour();
         colour.setHex("#00FF00");
-        colour.setName("Green");
+        colour.setName("Test Colour");
         return colour;
     }
 
     public static Hobby createHobby(){
         final Hobby hobby = new Hobby();
-        hobby.setName("Green");
+        hobby.setName("Test Hobby");
         return hobby;
     }
 
@@ -43,6 +53,21 @@ public final class TestUtils {
         person.setFavouriteColour(createColour());
         person.setDateOfBirth( LocalDate.of(2000, Month.JANUARY, 1));
         person.setHobbies(Arrays.asList(createHobby()));
+        return person;
+    }
+
+    public  static PersonDTO createPersonDTO() {
+        return createPersonDTO(FIRST_NAME, LAST_NAME);
+    }
+
+    private static PersonDTO createPersonDTO(String firstName, String lastName) {
+
+        final PersonDTO person = new PersonDTO();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setColourId("00298aa3-17ff-4f4a-a647-1fd06e4caab7");
+        person.setAge(45);
+        person.setHobbies(Arrays.asList("fba2f4e2-9dff-421b-9696-c6e3fa77653b", "fbc0b795-9f64-4f3c-b046-343bbc361e1f"));
         return person;
     }
 
--- src/test/java/com/hackerrank/assignment/controller/PersonControllerTest.java
+++ src/test/java/com/hackerrank/assignment/controller/PersonControllerTest.java
@@ -2,12 +2,24 @@ package com.hackerrank.assignment.controller;
 
 
 import com.hackerrank.assignment.AssignmentApplication;
+import com.hackerrank.assignment.TestUtils;
+import com.hackerrank.assignment.config.TestAuditingConfiguration;
+import com.hackerrank.assignment.domain.Colour;
+import com.hackerrank.assignment.domain.Hobby;
 import com.hackerrank.assignment.domain.Person;
 import com.hackerrank.assignment.dto.ListResponseDTO;
+import com.hackerrank.assignment.dto.PersonDTO;
 import com.hackerrank.assignment.dto.SearchCriteria;
+import com.hackerrank.assignment.exception.AssignmentException;
+import com.hackerrank.assignment.repository.ColourRepository;
+import com.hackerrank.assignment.repository.HobbyRepository;
+import com.hackerrank.assignment.repository.PersonRepository;
+import com.hackerrank.assignment.service.BaseListableService;
+import com.hackerrank.assignment.service.BaseService;
 import com.hackerrank.assignment.service.ColourService;
 import com.hackerrank.assignment.service.HobbyService;
 import com.hackerrank.assignment.service.PersonService;
+import com.hackerrank.assignment.service.PersonServiceImpl;
 import com.hackerrank.assignment.service.ServiceLocator;
 import com.hackerrank.assignment.util.AssignmentHelper;
 import lombok.extern.slf4j.Slf4j;
@@ -16,15 +28,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.omg.PortableInterceptor.SUCCESSFUL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -32,13 +55,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Filter;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.hackerrank.assignment.TestUtils.createPerson;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -47,8 +74,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Slf4j
 @RunWith(SpringRunner.class)
+@Sql({"classpath:import.sql"})
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootTest(classes = { AssignmentApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-
 public class PersonControllerTest {
 
     @LocalServerPort
@@ -58,17 +86,19 @@ public class PersonControllerTest {
     private WebApplicationContext context;
 
 
-
-
     private MockMvc mockMvc;
 
+
     @Before
-    public void setup() {
+    public void setup() throws AssignmentException {
         MockitoAnnotations.initMocks(this);
+
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
+
+
     }
 
     private String getRootUrl() {
@@ -76,22 +106,47 @@ public class PersonControllerTest {
     }
 
 
-    @WithMockUser("spring")
+    @WithMockUser
     @Test
     public void paginationTest() throws Exception {
 
-
-        ListResponseDTO<Person> responseDTO = new ListResponseDTO();
-        Person person = createPerson();
-        responseDTO.setData(Arrays.asList(person));
-        responseDTO.setTotalElement(1);
         LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
         requestParams.add("page", "0");
         requestParams.add("pageSize", "10");
         ResultActions result  = mockMvc.perform(get(getRootUrl() +"/pagination").params(requestParams));
         result.andExpect(status().isOk()).andExpect(jsonPath("$.status").value(AssignmentHelper.SUCCESS)).andDo(print());
 
+    }
+
+
+    @Test
+    @WithMockUser(username = "ahmed", roles = {"ADMIN"})
+    public void paginationAdminTest() throws Exception {
+
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("page", "0");
+        requestParams.add("pageSize", "10");
+        ResultActions result  = mockMvc.perform(get(getRootUrl() + "/admin/pagination").params(requestParams));
+        result.andExpect(status().isOk()).andExpect(jsonPath("$.status").value(AssignmentHelper.SUCCESS)).andDo(print());
 
     }
+
+
+    @Test
+    @WithMockUser(username="ahmed",roles={"ADMIN"})
+    public void createPersonTest() throws Exception {
+
+        PersonDTO dto = TestUtils.createPersonDTO();
+        String personJson = TestUtils.convertObjectToJson(dto);
+        ResultActions result  = mockMvc.perform(post(getRootUrl()).with(csrf()).with(user("ahmed").roles("ADMIN"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(personJson));
+
+        result.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").value(AssignmentHelper.SUCCESS))
+                .andExpect(jsonPath("$.data.firstName").value(dto.getFirstName())).andDo(print());
+
+    }
+
 
 }
